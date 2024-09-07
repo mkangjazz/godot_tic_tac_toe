7e2eb8e83e525cfdfd0b76e5f1682a65d9733ee9
Michael Kang <kaizenmike@gmail.com>
2024-09-07 17:33:04 -0600

    explore gridmaps and set up tile scene

---
+++ main.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+# track isPlaying or not
+
+# track games (best of 5, alternate who goes first)
+
+# track state of each tile
+# empty, X, O
+
+# track whose turn it is
+# player, CPU
+
+# win, lose, or tie
+# win conditions
+# if xxx or ooo
+
+# tie conditions
+# empty no moves left on board
+# no possible way to win (no combinations of 3)
+
+# CPU decides where to place an O
+
+# game timer?
---
+++ scene/tile.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+var isSelected:bool = false;
+
+# States
+
+# isSelected
+# texture?
+# Dynamically instance children conditionally
+# Empty, X, O
+
+# event emitter (signal)
+# should emit an event if
+# clicked or "enter/spacebar"
+# keyboard direction pressed:
+# wasd, arrow keys (navigate the grid)
